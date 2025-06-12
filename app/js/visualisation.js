148303f05e0ccc4a7b5a1703c7b32c5c99de130c
Antonin Lancesseur <antonin.lancesseur@universite-paris-saclay.fr>
2025-06-12 17:22:25 +0200

    nettoyage

--- app/js/visualisation.js
+++ app/js/visualisation.js
@@ -1,5 +1,5 @@
 d3.dsv(";", "./src/data/loto_201911.csv").then(function(data) {
-    const page = d3.select("#container")
+    const barNumChance = d3.select("#container")
 
     const numeros_chance = d3.rollup(data, v => d3.count(v, d => d.numero_chance), d => d.numero_chance)
     console.log(numeros_chance)
@@ -11,7 +11,6 @@ d3.dsv(";", "./src/data/loto_201911.csv").then(function(data) {
     marginTop=45
     height=1000;
     const scaleX = d3.scaleBand()
-        // .domain(numeros_chance.map(d => parseInt(d.numero_chance)).sort())
         .domain(["1","2","3","4","5","6","7","8","9", "10"])
         .range([marginLeft, width - marginRight])
         .padding(0.3);
@@ -20,13 +19,9 @@ d3.dsv(";", "./src/data/loto_201911.csv").then(function(data) {
         .domain([0, 120])
         .range([height, 0])
 
-        
-    // const scaleY = d3.scaleLinear()
-    // .domain([0, d3.max(data, (d) => d3.count(d.numero_chance))])
-
     
 
-    const svg = page.append("svg")
+    const svg = barNumChance.append("svg")
         .attr("width", width)
         .attr("height", height)
         .attr("viewBox", [0, 0, width, height])
@@ -39,13 +34,8 @@ d3.dsv(";", "./src/data/loto_201911.csv").then(function(data) {
         .data(numeros_chance)
         .join("rect")
         .attr("x", (d) => scaleX(d[0]))
-        // .attr("y", (d) => height-marginBottom-scaleY(d[1]))
-        // .attr("y", (d) => scaleY(d[1]))
         .attr("y", height)
-        // .attr("height", d => scaleY(0) - scaleY(d[1]))
         .attr("height", 0)
-        // .attr("y", (d) => scaleY(1000))
-        // .attr("height", (d) => scaleY(d[1]))
         .attr("width", scaleX.bandwidth())
 
     svg.append("g")
