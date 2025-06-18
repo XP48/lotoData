49fddd4c53c95bbac76a3bf407494d09cc526578
Antonin Lancesseur <antonin.lancesseur@universite-paris-saclay.fr>
2025-06-18 12:00:11 +0200

    adaptation des graphes

--- app/js/visualisation.js
+++ app/js/visualisation.js
@@ -5,11 +5,11 @@ d3.dsv(";", "./src/data/loto_201911.csv").then(function(data) {
     console.log(numeros_chance)
 
     marginLeft=50;
-    width=1000;
+    width=700;
     marginRight=60;
     marginBottom=20;
     marginTop=45
-    height=1000;
+    height=700;
     const scaleX = d3.scaleBand()
         .domain(["1","2","3","4","5","6","7","8","9", "10"])
         .range([marginLeft, width - marginRight])
@@ -42,7 +42,7 @@ d3.dsv(";", "./src/data/loto_201911.csv").then(function(data) {
         .attr("transform", `translate(${0},${height - marginBottom})`)
         .call(d3.axisBottom(scaleX).tickSizeOuter(0))
         .call(g => g.append("text")
-        .attr("x", width-(marginRight*1.5))
+        .attr("x", width-(marginRight*1.3))
         .attr("y", 20)
         .attr("fill", "currentColor")
         .attr("text-anchor", "start")
@@ -61,14 +61,22 @@ d3.dsv(";", "./src/data/loto_201911.csv").then(function(data) {
         .text("↑ Nombre d'apparition"));
 
     // Animation
-    svg.selectAll("rect")
-        .transition()
-        .duration(800)
-        .attr("y", d => scaleY(d[1]))
-        .attr("height", d => height - scaleY(d[1]))
-        .delay((d,i) => {console.log(i); return i*100})
-        .style("fill", "rgb(106,194,235)");
+    function anim() {
+        svg.selectAll("rect")
+            .style("fill", "#19163b")
+            .attr("y", height)
+            .attr("height", 0)
+        svg.selectAll("rect")
+            .transition()
+            .duration(1000)
+            .attr("y", d => scaleY(d[1]))
+            .attr("height", d => height - scaleY(d[1]))
+            .delay((d,i) => i*100)
+            .style("fill", "rgb(106,194,235)");
+    }
+    anim()
 
+    document.getElementById("barNumChance").addEventListener("click", anim)
 
     // 2ème graphique :
 
@@ -92,9 +100,9 @@ d3.dsv(";", "./src/data/loto_201911.csv").then(function(data) {
         .domain([0, 100])
         .range([height, 0])
 
-    
 
-    const svg2 = barNbWinDay.append("svg")
+
+    svg2 = barNbWinDay.append("svg")
         .attr("width", width)
         .attr("height", height)
         .attr("viewBox", [0, 0, width, height])
@@ -115,7 +123,7 @@ d3.dsv(";", "./src/data/loto_201911.csv").then(function(data) {
         .attr("transform", `translate(${0},${height - marginBottom})`)
         .call(d3.axisBottom(scaleX2).tickSizeOuter(0))
         .call(g => g.append("text")
-        .attr("x", width-(marginRight*2.5))
+        .attr("x", width-(marginRight*2))
         .attr("y", 20)
         .attr("fill", "currentColor")
         .attr("text-anchor", "start")
@@ -136,13 +144,87 @@ d3.dsv(";", "./src/data/loto_201911.csv").then(function(data) {
     // Animation
     function anim2() {
         svg2.selectAll("rect")
+            .style("fill", "#19163b")
+            .attr("y", height)
+            .attr("height", 0)
+        svg2.selectAll("rect")
             .transition()
             .duration(1000)
             .attr("y", d => scaleY(d[1]))
             .attr("height", d => height - scaleY(d[1]))
-            .delay((d,i) => {console.log(i); return i*100})
+            .delay((d,i) => i*100)
             .style("fill", "rgb(106,194,235)");
     }
     anim2()
+
+    document.getElementById("barNbWinDay").addEventListener("click", anim2)
+
+
+    // Graphique custom par rapport au numéro choisi
+    const percentageNumber = d3.select("#percentageNumber")
+    const number = 27
+    const count = d3.rollup(
+        data,
+        v => d3.sum(v, d => {
+          return [d.boule_1, d.boule_2, d.boule_3, d.boule_4, d.boule_5].filter(b => +b === number).length;
+        })
+      );
+    console.log(count)
+
+    // marginLeft=50;
+    // width=1000;
+    // marginRight=60;
+    // marginBottom=20;
+    // marginTop=45
+    // height=1000;
+    const scaleX3 = d3.scaleBand()
+        .domain(["LUNDI", "MERCREDI", "SAMEDI"])
+        .range([marginLeft, width - marginRight])
+        .padding(0.3);
+
+    const scaleY3 = d3.scaleLinear()
+        .domain([0, 100])
+        .range([height, 0])
+
+
+
+    svg2 = barNbWinDay.append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .attr("viewBox", [0, 0, width, height])
+        .attr("style", "max-width: 100%; height: auto;");
+    
+    svg2.append("g")
+        .attr("fill", "#19163b")
+        .attr("transform", `translate(0, ${-marginBottom})`)
+        .selectAll()
+        .data(jours)
+        .join("rect")
+        .attr("x", (d) => scaleX2(d[0]))
+        .attr("y", height)
+        .attr("height", 0)
+        .attr("width", scaleX2.bandwidth())
+
+    svg2.append("g")
+        .attr("transform", `translate(${0},${height - marginBottom})`)
+        .call(d3.axisBottom(scaleX2).tickSizeOuter(0))
+        .call(g => g.append("text")
+        .attr("x", width-(marginRight*2))
+        .attr("y", 20)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .text("→ Jour de tirage"));
+        
+
+    svg2.append("g")
+    .attr("transform", `translate(${marginLeft},${-marginBottom})`)
+    .call(d3.axisLeft(scaleY2).tickFormat((y) => (y).toFixed()))
+    .call(g => g.select(".domain").remove())
+    .call(g => g.append("text")
+        .attr("x", -marginLeft)
+        .attr("y", marginTop)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .text("↑ Nombre de gagnants"));
 
 });
