7b8d5c4675050d8d0d6b794a2c838630b016be1d
Matthieu <matthieu.gaymay@gmail.com>
2025-06-19 16:40:58 +0200

    maj du site 👍

--- app/js/grille.js
+++ app/js/grille.js
@@ -3,31 +3,43 @@ const numero_chance = document.getElementById("numero-chance");
 
 const numeroDisabled = document.getElementById("numero2");
 const numero_chanceDisabled = document.getElementById("numero-chance2");
-const numeroDisabled2 = document.getElementById("numero2");
-const numero_chanceDisabled2 = document.getElementById("numero-chance2");
-const numeroDisabled3 = document.getElementById("numero2");
-const numero_chanceDisabled3 = document.getElementById("numero-chance2");
-const numeroDisabled4 = document.getElementById("numero2");
-const numero_chanceDisabled4 = document.getElementById("numero-chance2");
+const numeroDisabled2 = document.getElementById("numero3");
+const numero_chanceDisabled2 = document.getElementById("numero-chance3");
+const numeroDisabled3 = document.getElementById("numero4");
+const numero_chanceDisabled3 = document.getElementById("numero-chance4");
+const numeroDisabled4 = document.getElementById("numero5");
+const numero_chanceDisabled4 = document.getElementById("numero-chance5");
 
 const numListe = [];
 const numChanceListe = [];
 const MAX_NUM = 5;
 const MAX_NUM_CHANCE = 1;
 
+const boules = [
+  document.getElementById("boule1"),
+  document.getElementById("boule2"),
+  document.getElementById("boule3"),
+  document.getElementById("boule4"),
+  document.getElementById("boule5"),
+  document.getElementById("boule6"),
+];
+
+const somme = document.getElementById("somme");
+const date = document.getElementById("date");
+
 numero.addEventListener("click", selected);
 numero_chance.addEventListener("click", selected);
 
 async function checkCombinaison() {
   const gains = new Map([
-    ["0n1c", 2],
+    ["0n1c", 2], //0 numero 1 chance
     ["1n1c", 2],
     ["2n0c", 5],
-    ["2n1c", 5],
+    ["2n1c", 7],
     ["3n0c", 10],
-    ["3n1c", 10],
+    ["3n1c", 12],
     ["4n0c", 1086],
-    ["4n1c", 1086],
+    ["4n1c", 1088],
     ["5n0c", 102634],
     ["5n1c", 5701258],
   ]);
@@ -52,13 +64,13 @@ async function checkCombinaison() {
       }
     }
 
-    let bonneChance;
+    let nbChance;
     if (chance === combiChance) {
-      bonneChance = 1;
+      nbChance = 1;
     } else {
-      bonneChance = 0;
+      nbChance = 0;
     }
-    const key = `${nbCorrects}n${bonneChance}c`;
+    const key = `${nbCorrects}n${nbChance}c`;
 
     if (gains.has(key)) {
       const gain = gains.get(key);
@@ -75,6 +87,8 @@ async function checkCombinaison() {
   }
 
   if (meilleurGain > 0) {
+    somme.innerText = meilleurGain + "€";
+    date.innerText = ``;
     console.log(
       `Tu aurais gagner ${meilleurGain}€ avec ${
         meilleureCombinaison.key
@@ -97,6 +111,19 @@ for (let index = 1; index <= 50; index++) {
   num.classList.add("case");
   num.innerText = index;
   numero.appendChild(num);
+
+  let copy = num.cloneNode(true);
+  copy.classList.add("disabled");
+  numeroDisabled.appendChild(copy);
+  let copy2 = num.cloneNode(true);
+  copy2.classList.add("disabled");
+  numeroDisabled2.appendChild(copy2);
+  let copy3 = num.cloneNode(true);
+  copy3.classList.add("disabled");
+  numeroDisabled3.appendChild(copy3);
+  let copy4 = num.cloneNode(true);
+  copy4.classList.add("disabled");
+  numeroDisabled4.appendChild(copy4);
 }
 
 for (let index = 1; index <= 10; index++) {
@@ -105,6 +132,19 @@ for (let index = 1; index <= 10; index++) {
   num.classList.add("chance");
   num.innerText = index;
   numero_chance.appendChild(num);
+
+  let copy = num.cloneNode(true);
+  copy.classList.add("disabled");
+  numero_chanceDisabled.appendChild(copy);
+  let copy2 = num.cloneNode(true);
+  copy2.classList.add("disabled");
+  numero_chanceDisabled2.appendChild(copy2);
+  let copy3 = num.cloneNode(true);
+  copy3.classList.add("disabled");
+  numero_chanceDisabled3.appendChild(copy3);
+  let copy4 = num.cloneNode(true);
+  copy4.classList.add("disabled");
+  numero_chanceDisabled4.appendChild(copy4);
 }
 
 function selected(event) {
@@ -138,6 +178,13 @@ function selected(event) {
     }
   }
   if (numChanceListe.length == MAX_NUM_CHANCE && numListe.length == MAX_NUM) {
+    for (let i = 0; i < numListe.length; i++) {
+      boules[i].innerText = numListe[i];
+      console.log(`boule${i}`, numListe[i]);
+    }
+    boules[5].innerText = numChanceListe[0];
+    console.log(numChanceListe);
+    console.log(numListe);
     loadCSV();
     checkCombinaison();
   }
